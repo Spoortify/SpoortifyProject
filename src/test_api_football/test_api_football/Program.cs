4eb0ead2357746e037a929500f26503552c0bebe
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-11-14 13:11:25 +0100

    rugby

--- src/test_api_football/test_api_football/Program.cs
+++ src/test_api_football/test_api_football/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace test_api_football
+﻿using System.Text.Json;
+
+namespace test_api_football
 {
     internal class Program
     {
@@ -34,5 +36,33 @@
         //UEFA Europa Conference League -> 848
         //liga portoghese -> 94
         //liga turca -> 203
+
+
+        #region rugby
+//        HttpClient client = new HttpClient();
+//        client.BaseAddress = new Uri("https://v1.rugby.api-sports.io/");
+//        client.DefaultRequestHeaders.Add("x-rapidapi-key", "7169e21806353dcad1a1592a2b7043bd");
+//            client.DefaultRequestHeaders.Add("x-rapidapi-host", "v3.football.api-sports.io");
+
+//            var response = await client.GetAsync("/games/?date=2023-11-12");
+//            if (response.IsSuccessStatusCode)
+//            {
+//                var content = await response.Content.ReadAsStringAsync();
+//        var rugby = JsonSerializer.Deserialize<Rugby>(content);
+
+//                foreach (var game in rugby.Games)
+//                {
+//                    Console.WriteLine($"Date: {game.Date}, Time: {game.Time}, Country: {game.Country}, League: {game.League}");
+//                    Console.WriteLine($"Home Team: {game.HomeTeam}, Away Team: {game.AwayTeam}");
+//                    Console.WriteLine($"Score: {game.HomeScore} - {game.AwayScore}");
+//                    Console.WriteLine();
+//                }
+//}
+//            else
+//{
+//    Console.WriteLine("Error: {0}", response.StatusCode);
+//}
+
+        #endregion
     }
 }
