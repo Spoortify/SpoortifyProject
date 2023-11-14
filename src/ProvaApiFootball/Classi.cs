33a440a26bab76440d79793138a705fa274734f7
cristiancagliani <cristian.cagliani@issgreppi.it>
2023-11-14 10:57:24 +0100

    prova API formula 1

---
+++ src/ProvaApiFootball/Classi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaApiFootball
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Driver
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string abbr { get; set; }
+        public int? number { get; set; }
+        public string image { get; set; }
+    }
+
+    public class Parameters
+    {
+        public string season { get; set; }
+    }
+
+    public class Response
+    {
+        public int position { get; set; }
+        public Driver driver { get; set; }
+        public Team team { get; set; }
+        public int? points { get; set; }
+        public int wins { get; set; }
+        public int? behind { get; set; }
+        public int season { get; set; }
+    }
+
+    public class Formula1
+    {
+        public string get { get; set; }
+        public Parameters parameters { get; set; }
+        public List<object> errors { get; set; }
+        public int results { get; set; }
+        public List<Response> response { get; set; }
+    }
+
+    public class Team
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string logo { get; set; }
+    }
+
+
+}
---
+++ src/ProvaApiFootball/Program.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System.Net.Http.Json;
+
+namespace ProvaApiFootball
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://v1.formula-1.api-sports.io");
+            client.DefaultRequestHeaders.Add("x-rapidapi-key", "5a00e1120640f33486efef17ad7818e6");
+            client.DefaultRequestHeaders.Add("x-rapidapi-host", "v1.formula-1.api-sports.io");
+            var response = await client.GetAsync("/rankings/drivers?season=2012");
+            Formula1 lista = await response.Content.ReadFromJsonAsync<Formula1>();
+            lista.response.ForEach(r => Console.WriteLine($"{r.driver.name} {r.points}"));
+        }
+    }
+}
