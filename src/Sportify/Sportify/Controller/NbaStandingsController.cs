3a4b186193824029b0b631654b4e22541c0c6f7d
umbecol <umberto.colombo@issgreppi.it>
2023-11-14 18:11:46 +0100

    fix: 0.3.1 nba
    collegamento controller-view

--- src/Sportify/Sportify/Controller/NbaStandingsController.cs
+++ src/Sportify/Sportify/Controller/NbaStandingsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Sportify.Controller
 {
-    public class NbaStandingsController
+    public partial class NbaStandingsController : ObservableObject
     {
+
     }
 }
