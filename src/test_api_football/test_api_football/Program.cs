4264dd03e0eb61afe7b2ca9f9669080bbad46945
Giorgio Citterio <giorgio.citterio@issgreppi.it>
2023-11-14 10:53:59 +0100

    added api-football request

---
+++ src/test_api_football/test_api_football/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace test_api_football
+{
+    internal class Program
+    {
+        static HttpClient client = new();
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://v3.football.api-sports.io/");
+            client.DefaultRequestHeaders.Add("x-rapidapi-key", "7169e21806353dcad1a1592a2b7043bd");
+            client.DefaultRequestHeaders.Add("x-rapidapi-host", "v3.football.api-sports.io");
+
+            var response = await client.GetAsync("/fixtures");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine("Error: {0}", response.StatusCode);
+            }
+        }
+    }
+}
