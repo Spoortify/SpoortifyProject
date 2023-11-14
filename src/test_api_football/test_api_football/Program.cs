0bec8e40ac6870aca952598a9099bd9da1ace15c
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-11-14 11:50:20 +0100

    add leagues id

--- src/test_api_football/test_api_football/Program.cs
+++ src/test_api_football/test_api_football/Program.cs
@@ -10,7 +10,7 @@
             client.DefaultRequestHeaders.Add("x-rapidapi-key", "7169e21806353dcad1a1592a2b7043bd");
             client.DefaultRequestHeaders.Add("x-rapidapi-host", "v3.football.api-sports.io");
 
-            var response = await client.GetAsync("/fixtures");
+            var response = await client.GetAsync("/injuries");
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
@@ -21,5 +21,18 @@
                 Console.WriteLine("Error: {0}", response.StatusCode);
             }
         }
+
+        //League ids:
+        //Serie A -> 71
+        //Premier League -> 39
+        //Ligue 1 -> 61
+        //Bundesliga -> 78
+        //La liga -> 140
+        //UEFA Champions League -> 2
+        //UEFA Europa League -> 3
+        //Eredivise -> 88
+        //UEFA Europa Conference League -> 848
+        //liga portoghese -> 94
+        //liga turca -> 203
     }
 }
