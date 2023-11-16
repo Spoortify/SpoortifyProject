6e98b88f6c0e0902915bee1263c80a3d3c0fbb7f
cristiancagliani <cristian.cagliani@issgreppi.it>
2023-11-16 19:18:26 +0100

    0.15.0 create homepage

---
+++ src/Sportify/Sportify/Controller/HomePageController.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sportify.Controller
+{
+    public partial class HomePageController : ObservableObject
+    {
+        [ObservableProperty]
+        int rotation = 90;
+        [ObservableProperty]
+        bool isVisible = false;
+
+        [RelayCommand]
+        public void ViewMenu()
+        {
+            Rotation = Rotation == 90 ? 270 : 90;
+            IsVisible = !IsVisible;
+        }
+    }
+}
