fbf969804099c9971ec4dc7e0475477a126386a0
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-11-23 22:52:27 +0100

    feat: 0.26.0 add window size max

--- src/Sportify/Sportify/App.xaml.cs
+++ src/Sportify/Sportify/App.xaml.cs
@@ -87,5 +87,35 @@ namespace Sportify
             };
             return id.ToString();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            Window window = base.CreateWindow(activationState);
+            window.Activated += Window_Activated;
+            return window;
+        }
+
+        private async void Window_Activated(object sender, EventArgs e)
+        {
+#if WINDOWS
+        const int DefaultWidth = 1920;
+        const int DefaultHeight = 1080;
+
+        var window = sender as Window;
+
+        // change window size.
+        window.Width = DefaultWidth;
+        window.Height = DefaultHeight;
+
+        // give it some time to complete window resizing task.
+        await window.Dispatcher.DispatchAsync(() => { });
+
+        var disp = DeviceDisplay.Current.MainDisplayInfo;
+
+        // move to screen center
+        window.X = (disp.Width / disp.Density - window.Width) / 2;
+        window.Y = (disp.Height / disp.Density - window.Height) / 2;
+#endif
+        }
     }
 }
